c73101b17a405d5a8d345a6408728fc1867b582f
fatima rahimi <112756954+iamfatimarahimi@users.noreply.github.com>
2023-08-26 12:52:15 +0330

    Add files via upload

---
+++ space/script.js
@@ -0,0 +1,7 @@
+setInterval(function myFunction(){
+  let d = new Date();
+  document.getElementById("demo").innerHTML=
+  d.getHours() + ":" +
+  d.getMinutes() + ":" +
+  d.getSeconds();
+}, 5000);
